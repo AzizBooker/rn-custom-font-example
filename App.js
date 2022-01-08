dd5edc230c9904faa586c084a912532a43948335
AzizBooker <bookeraziz@gmail.com>
2022-01-07 19:47:56 -0500

    added text and learn more button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text,StyleSheet,ScrollView } from 'react-native';
+import { View,StatusBar, Text,StyleSheet,ScrollView,Pressable } from 'react-native';
 import { useFonts, Inter_900Black,Inter_300Light } from '@expo-google-fonts/inter';
 
 export default function App() {
@@ -10,7 +10,14 @@ export default function App() {
 
    
     return (
-      <ScrollView style={styles.container}>
+      
+      <ScrollView
+      contentContainerStyle={styles.container}
+       bounces={false}
+      
+       indicatorStyle='white'
+      >
+        <StatusBar hidden/>
         <Text style={styles.heading}>Inter Black</Text>
         <Text style={styles.text}> 
         Do ut sit veniam do aute tempor. Ipsum est consequat cupidatat cupidatat ipsum incididunt deserunt deserunt culpa anim. Deserunt et laborum ea esse 
@@ -32,9 +39,18 @@ export default function App() {
         sunt mollit irure occaecat. Mollit elit est elit ex pariatur cupidatat occaecat do exercitation voluptate. 
         Lorem nisi voluptate irure nostrud dolor laboris ad aliquip nostrud dolor fugiat in.
         </Text>
-        
-         
+        <Text style={styles.text}> 
+        Do ut sit veniam do aute tempor. Ipsum est consequat cupidatat cupidatat ipsum incididunt deserunt deserunt culpa anim. Deserunt et laborum ea esse 
+        sunt mollit irure occaecat.
+        </Text>
+        <Pressable 
+        onPress={()=>console.warn("Hello")}
+
+        >
+        <Text style={[styles.text,styles.textCTA]}>Learn More</Text>
+        </Pressable>
       </ScrollView>
+    
     );
   
 }
@@ -42,18 +58,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container:{
       padding:20,
-      paddingLeft:12,
-      paddingRight:12,
+      paddingLeft:20,
+      paddingRight:20,
+
+      backgroundColor:"#111"
   },
+
   heading:{
     fontFamily: 'Inter_900Black',
-     fontSize: 40
+     fontSize: 40,
+     color:'#fff'
   },
   text:{
     fontFamily: 'Inter_300Light',
     fontSize: 16,
     paddingTop:6,
-    paddingBottom:6
+    paddingBottom:6,
+    color:"#ffffffdd"
+  },
+  textCTA:{
+    marginTop:20,
+    textDecorationLine:'underline',
+    textAlign:"right"
   }
 })
 
